0eeb1c28f5d5b3b962a02ab91294971d115838f2
Sıla <silaozyesil@gmail.com>
2020-02-23 12:25:11 +0300

    GpsExample

---
+++ GpsProject2/GpsProject2/GpsProject2/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Geolocator;
+
+
+namespace GpsProject2
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnGetLocation.Clicked += BtnGetLocation_Clicked;
+        }
+
+        private async void BtnGetLocation_Clicked(object sender, EventArgs e)
+        {
+            await RetreiveLocation();
+        }
+
+        private async Task RetreiveLocation()
+        {
+            var locator = CrossGeolocator.Current;
+            locator.DesiredAccuracy = 20;
+
+            var position = await locator.GetPositionAsync(TimeSpan.FromSeconds(10), null);
+
+            txtLat.Text = "Latitude: " + position.Latitude.ToString();
+            txtLong.Text = "Longitude: " + position.Longitude.ToString();
+
+        }
+    }
+}
